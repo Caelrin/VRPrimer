d67c14f20a05cd16f32c390fd1ff650cde4333a2
Gabbie <caelrin@experimentalcork.com>
2017-01-05 12:46:22 -0700

    Initial commit.

---
+++ Assets/Scripts/DriveController.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DriveController : MonoBehaviour {
+  public GameObject cameraRig;
+  public GameObject Lever;
+
+  private bool connectedToLever = false;
+  private bool pullingLever = false;
+  private static bool controllerLock = false;
+  private static float speed = 0;
+
+  private SteamVR_TrackedObject controller;
+
+  // Use this for initialization
+  void Start () {
+    controller = GetComponent<SteamVR_TrackedObject>();
+  }
+
+  void OnTriggerEnter(Collider collider) {
+    if (collider.gameObject.layer == LayerMask.NameToLayer("Lever")) {
+      Debug.Log("Collided with lever");
+      connectedToLever = true;
+    }
+  }
+
+  void OnTriggerExit(Collider collider) {
+    if (collider.gameObject.layer == LayerMask.NameToLayer("Lever")) {
+      Debug.Log("Left connection from lever");
+      connectedToLever = false;
+    }
+  }
+
+  // Update is called once per frame
+  void Update() {
+    if (pullingLever) {
+      Lever.transform.LookAt(new Vector3(transform.position.x, transform.position.y, Lever.transform.position.z));
+      speed = transform.position.x - Lever.transform.position.x;
+      speed *= .5f;
+    }
+    if (speed < .03 && speed > -.03) {
+      speed = 0;
+    }
+    cameraRig.transform.position += new Vector3(speed, 0, 0);
+
+    if (!controllerLock && connectedToLever &&
+        SteamVR_Controller.Input(checked((int) controller.index)).GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+      Debug.Log("Pulling Lever");
+      pullingLever = true;
+    }
+    if (SteamVR_Controller.Input(checked((int)controller.index)).GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      if (pullingLever) {
+        controllerLock = false;
+      }
+      Debug.Log("Releasing Lever");
+      pullingLever = false;
+    }
+  }
+}
---
+++ Assets/Scripts/PullYourself.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullYourself : MonoBehaviour {
+
+  public GameObject cameraRig;
+
+  private SteamVR_TrackedObject controller;
+  private bool pulling = false;
+  private Vector3 basePosition;
+  private Vector3 baseRigPosition;
+
+  // Use this for initialization
+  void Start () {
+	  controller = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	  if (pulling) {
+	    Vector3 difference = basePosition - transform.localPosition;
+	    difference.x = difference.x * 5.0f;
+      difference.z = difference.z * 5.0f;
+      difference.y = 0;
+
+      cameraRig.transform.position = baseRigPosition + difference;
+	  }
+
+    if (SteamVR_Controller.Input(checked((int)controller.index)).GetPressDown(SteamVR_Controller.ButtonMask.Trigger)) {
+      Debug.Log("Starting to grab pull");
+	    basePosition = transform.localPosition;
+	    baseRigPosition = cameraRig.transform.position;
+      pulling = true;
+	  }
+    if (SteamVR_Controller.Input(checked((int)controller.index)).GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+    {
+      Debug.Log("Releasing grab pull");
+      pulling = false;
+    }
+  }
+}
